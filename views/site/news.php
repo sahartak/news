4bf4d01e50abbda81a94decefc7c623d51cd540d
grigory-web <grigori.podosyan@gmail.com>
2015-11-03 12:51:00 +0400

    news styles

--- views/site/news.php
+++ views/site/news.php
@@ -6,12 +6,13 @@ $this->title = $news['title'];
 
 ?>
 <div class="news-page">
-	<div class="row">
-		<!--<h3 class="pull-right"><?/*=date('d.m.Y H:i', strtotime($news['published']))*/?></h3>-->
-		<h1><?=$news['title']?></h1>
-	</div>
-	<div class="row">
-		<h2><img src="/uploads/<?=$news['id']?>.png" alt="<?=$news['title']?>" title="<?=$news['title']?>"></h2>
-		<?=$news['content']?>
-	</div>
+    <div class="col-md-8 news-content">
+        <!--<h3 class="pull-right"><?/*=date('d.m.Y H:i', strtotime($news['published']))*/?></h3>-->
+        <h1><?=$news['title']?></h1>
+        <h2><img src="/uploads/<?=$news['id']?>.png" alt="<?=$news['title']?>" title="<?=$news['title']?>"></h2>
+        <?=$news['content']?>
+    </div>
+    <div class="col-md-3 news-right-block">
+        dssdg
+    </div>
 </div>
